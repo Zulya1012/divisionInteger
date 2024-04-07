4d3b6c0f8f651c0a1e582325cf79b8466f1b9597
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-04-07 19:15:07 +0400

    Add files via upload

---
+++ Method1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Method1 { 
+ public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Input an integer:");
+        int n = s.nextInt();
+        System.out.print("Factors of 3 of the said integer:");
+        test(n);
+        }
+
+public static void test(int n){
+    System.out.print(n + " = ");
+    int result = n;
+    while (result % 3 == 0){
+        System.out.print("3 * ");
+        result = result / 3;
+    }
+    System.out.print(result);
+}
+}
